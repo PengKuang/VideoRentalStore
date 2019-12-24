c2cadaa6a239fe46c82d0a361a211adb966dc41a
PengKuang <kap.kuang@gmail.com>
2019-12-25 05:15:44 +1100

    clean up

--- app.py
+++ app.py
@@ -2,23 +2,23 @@ from flask import Flask
 from flask import render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-# from flask_validator import ValidateInteger, ValidateString, ValidateEmail
 
 app = Flask(__name__)
 app.config['SQLACHEMY_DATABASE_URI'] = 'sqlite:///video.db'
+# db.init_app(app)
 db = SQLAlchemy(app)
 
 class Film(db.Model):
     __tablename__ = "film"
 
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
+    name = db.Column(db.String(200), nullable=False)
     category = db.Column(db.String(20))
-    completed = db.Column(db.Integer, default=0)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    available = db.Column(db.Integer, default=0)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return '<Task %r>' % self.content
+        return '<Film %r>' % self.name
 
 class Rental(db.Model):
     __tablename__ = "rental"
@@ -45,53 +45,75 @@ class Customer(db.Model):
     def __repr__(self):
         return '<Customer %r>' % self.id
 
-    # @classmethod
-    # def __delcare_last_(cls):
-    #     ValidateEmail(Customer.email, true, true, "The e-mail is not valid. Please check it")    
-
 @app.route('/', methods=['POST','GET'])
 def index():
     if request.method == 'POST':
-        task_content = request.form['content']
-        new_task = Film(content=task_content)
+        film_name = request.form['fname']
+        new_film = Film(name=film_name)
 
         try:
-            db.session.add(new_task)
+            db.session.add(new_film)
             db.session.commit()
             return redirect('/')
 
         except:
-            return 'there was an issue adding the task'
+            return 'there was an issue adding the film'
 
     else:
-        tasks = Film.query.order_by(Film.date_created).all()
-        return render_template('index.html', tasks=tasks)
+        films = Film.query.all()
+        return render_template('index.html', films=films)
 
 @app.route('/delete/<int:id>')
 def delete(id):
-    task_to_delete = Film.query.get_or_404(id)
+    film_to_delete = Film.query.get_or_404(id)
 
     try:
-        db.session.delete(task_to_delete)
+        db.session.delete(film_to_delete)
         db.session.commit()
         return redirect('/')
     except:
-        return 'there was a problem deleting the task'
+        return 'there was a problem deleting the film'
 
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
-    task = Film.query.get_or_404(id)
+    film = Film.query.get_or_404(id)
 
     if request.method == 'POST':
-        task.content = request.form['content']
+        film.name = request.form['fname']
 
         try:
             db.session.commit()
             return redirect('/')
         except:
-            return 'there was an issue updating the task'
+            return 'there was an issue updating the film'
     else:
-        return render_template('update.html', task=task)
+        return render_template('update.html', film=film)
+
+@app.route('/rent/<int:id>', methods=['GET', 'POST'])
+def rent(id):
+    film = Film.query.get_or_404(id)
+
+    if request.method == 'POST':
+        nfilm_name = film.name
+        ncust_id = request.form['customerid']
+        nstart_date = request.form['startdate']
+        nend_date = request.form['enddate']
+
+        new_rental = Rental(film_name=nfilm_name, cust_id=ncust_id, start_date=nstart_date, end_date=nend_date)
+
+        try:
+            db.session.add(new_rental)
+            db.session.commit()
+            return redirect('/rentals')
+        except:
+            return 'there was an issue adding the rental'
+    else:
+        return render_template('rent.html', film=film)
+
+@app.route('/rentals', methods=['GET'])
+def get_all_rentals():
+    rentals = Rental.query.order_by(Rental.end_date).all()
+    return render_template('rentals.html', rentals=rentals)
 
 @app.route('/customers', methods=['POST','GET'])
 def customer():
@@ -110,35 +132,8 @@ def customer():
             return 'there was an issue adding the customer'
 
     else:
-        tasks = Customer.query.all()
-        return render_template('customers.html', tasks=tasks)
-
-@app.route('/rent/<int:id>', methods=['GET', 'POST'])
-def rent(id):
-    film = Film.query.get_or_404(id)
-
-    if request.method == 'POST':
-        nfilm_name = film.content
-        ncust_id = request.form['customerid']
-        nstart_date = request.form['startdate']
-        nend_date = request.form['enddate']
-
-        new_rental = Rental(film_name=nfilm_name, cust_id=ncust_id, start_date=nstart_date, end_date=nend_date)
-
-        try:
-            db.session.add(new_rental)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'there was an issue adding the rental'
-    else:
-        return render_template('rent.html', task=film)
-
-@app.route('/rentals', methods=['GET'])
-def get_all_rentals():
-    rentals = Rental.query.order_by(Rental.end_date).all()
-    return render_template('rentals.html', tasks=rentals)
-
+        customers = Customer.query.all()
+        return render_template('customers.html', customers=customers)
 
 @app.before_first_request
 def create_tables():
